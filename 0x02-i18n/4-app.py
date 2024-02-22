390fa02615ee2d28b6e489974816d724fe8ab1fe
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-22 04:07:15 +0200

    locale with URL parameter

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Babel Configuration"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ Get locale from request """
+    if "locale" in request.args.keys():
+        return request.args['locale']
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", methods=["GET"])
+def index() -> str:
+    """ GET / """
+    return render_template("4-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
