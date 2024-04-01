80884a546878434c3c349ee504562d695ce3c45a
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-04-01 22:30:03 +0200

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""docs"""
+"""0x00. Pagination"""
 
 
 def index_range(page: int, page_size: int) -> tuple:
-    """docs"""
+    """index_range"""
     return ((page - 1) * page_size, page * page_size)
